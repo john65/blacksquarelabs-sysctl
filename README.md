d8ba7c97255a79ce5b5a432590aac0b569e73f34
John Coleman <john.coleman@yale.edu>
2016-08-16 22:04:50 -0400

    set net.ipv4.tcp_challenge_ack_limit=1073741823

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # blacksquarelabs-sysctl
 
-TODO: Enter the cookbook description here.
+Wrapper cookbook for sysctl
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#
+include_recipe 'sysctl::apply'
+# https://blogs.akamai.com/2016/08/vulnerability-in-the-linux-kernels-tcp-stack-implementation.html
+node.default['sysctl']['params']['net']['ipv4']['tcp_challenge_ack_limit'] = 1073741823
