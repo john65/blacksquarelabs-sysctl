8c90a8e2683df731f3f87195f26cec4d501a2e26
John Coleman <john.coleman@yale.edu>
2017-01-30 12:28:06 -0500

    remove ipv6 disable. doesn't work

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,3 @@
 include_recipe 'sysctl::apply'
 # https://blogs.akamai.com/2016/08/vulnerability-in-the-linux-kernels-tcp-stack-implementation.html
 node.default['sysctl']['params']['net']['ipv4']['tcp_challenge_ack_limit'] = 1073741823
-# disable ipv6
-node.default['sysctl']['params']['net']['ipv6']['conf']['all']['disable_ipv6'] = 1
-node.default['sysctl']['params']['net']['ipv6']['conf']['default']['disable_ipv6'] = 1
-node.default['sysctl']['params']['net']['ipv6']['conf']['lo']['disable'] = 1
